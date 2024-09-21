24f6a25c6c54f6a2bafc3360357ecbb9de9b852f
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-21 20:52:45 +0530

    Drawer navigatior correctly work

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Sidenav from '../Sidenav'
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 
 function About() {
   return (
-    <div>About</div>
+    <>
+
+        <Sidenav/>
+        <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3,paddingLeft:27,position:'absolute' }}
+        >
+        <Toolbar />
+        <h1>About Page</h1>
+        </Box>
+
+    </>
   )
 }
 
